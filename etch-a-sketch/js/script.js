3b4a2fcf22c5dd4b1506a605740f1f00ea5fdf97
Ivaldson <ivaldson.martins@gmail.com>
2023-02-15 11:32:23 -0300

    feat: added hovering effect
    changes to the etch-a-sketch project
    
    added event listeners to each div on the square
    and changing class when it's hovered

--- etch-a-sketch/js/script.js
+++ etch-a-sketch/js/script.js
@@ -2,5 +2,12 @@ const container = document.querySelector('#container');
 
 for (let index = 0; index < 256; index++) {
     const div = document.createElement('div');
+    div.classList.add('blank');
     container.appendChild(div);
 }
+const divsInsideContainer = document.querySelectorAll('.blank');
+divsInsideContainer.forEach(div => {
+    div.addEventListener('mouseover', () => {
+        div.classList.add('change-background-color-on-hover');
+    })
+});
