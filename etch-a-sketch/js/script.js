e2403efc4a6abb12dc988030ba302644590eca2b
Ivaldson <ivaldson.martins@gmail.com>
2023-02-15 10:44:34 -0300

    feat: creating the grid with css
    changes to the etch-a-sketch project
    
    using css-grid to make the square of divs

--- etch-a-sketch/js/script.js
+++ etch-a-sketch/js/script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('#container');
 
-for (let index = 0; index < 16; index++) {
+for (let index = 0; index < 256; index++) {
     const div = document.createElement('div');
     container.appendChild(div);
 }
