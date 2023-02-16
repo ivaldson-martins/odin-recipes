527aeeaabddc5b4354e16ce0dffb1fff85bc8dfc
Ivaldson <ivaldson.martins@gmail.com>
2023-02-16 11:40:45 -0300

    feat: new grid sizes available
    changes to the etch-a-sketch project
    
    put the process of creating divs and drawing them in two diferent
    functions and added the process of clearing the divs inside the
    container  when the grid size button is clicked

--- etch-a-sketch/js/script.js
+++ etch-a-sketch/js/script.js
@@ -1,17 +1,9 @@
 const container = document.querySelector('#container');
 const buttonSquareSize = document.querySelector('#button-square-size');
-
-
-buttonSquareSize.addEventListener('click', () => {
-    let promptResult = prompt('Write the number of squares per side.(maximum of 100)');
-    console.log(promptResult);
-});
-
-/*
-função para detectar se o mouse esta'sendo presionado
-*/
+let promptResult = 256;
 var mousePresionado = false;
 
+/*função para detectar se o mouse esta'sendo presionado*/
 document.addEventListener('mousedown', () => {
     mousePresionado = true;
 });
@@ -19,25 +11,39 @@ document.addEventListener('mouseup', () => {
     mousePresionado = false;
 });
 
-
-
-
-for (let index = 0; index < 256; index++) {
-    const div = document.createElement('div');
-    div.classList.add('blank');
-    container.appendChild(div);
-}
-const divsInsideContainer = document.querySelectorAll('.blank');
-divsInsideContainer.forEach(div => {
-
-    div.addEventListener('mousedown', () => {
-        div.classList.add('change-background-color-on-hover');
-    });
-
-    div.addEventListener('mouseover', () => {
-        if (mousePresionado){
+function drawDivs(numberOfDivs){
+    for (let index = 0; index < (numberOfDivs*numberOfDivs); index++) {
+        const div = document.createElement('div');
+        div.classList.add('blank');
+        div.classList.remove('change-background-color-on-hover');
+        container.appendChild(div);
+    }
+};
+function drawnColors() {
+    const divsInsideContainer = document.querySelectorAll('.blank');
+    divsInsideContainer.forEach(div => {
+    
+        div.addEventListener('mousedown', () => {
             div.classList.add('change-background-color-on-hover');
-        };
-
-    })
+        });
+    
+        div.addEventListener('mouseover', () => {
+            if (mousePresionado){
+                div.classList.add('change-background-color-on-hover');
+            };
+    
+        })
+    });
+}
+drawDivs(16);
+drawnColors();
+buttonSquareSize.addEventListener('click', () => {
+    let divSize = '1fr ';
+    let promptResult = prompt('Write the number of squares per side.(maximum of 100)');
+    container.style.gridTemplateColumns = divSize.repeat(promptResult);
+    container.style.gridTemplateRows = divSize.repeat(promptResult);
+    container.replaceChildren();
+    drawDivs(promptResult);
+    drawnColors();
 });
+
