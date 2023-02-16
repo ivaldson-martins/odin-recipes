ffeef713fe089f9cc04980cbfca8b6b1e12cc311
Ivaldson <ivaldson.martins@gmail.com>
2023-02-16 09:37:25 -0300

    feat: added drawn feature
    changes to the etch-a-sketch project
    
    used variable to detect mouse held down added 2 event listeners to each
    div inside the square to detect point draw and mouse held drawn

--- etch-a-sketch/js/script.js
+++ etch-a-sketch/js/script.js
@@ -1,5 +1,20 @@
 const container = document.querySelector('#container');
 
+/*
+função para detectar se o mouse esta'sendo presionado
+*/
+var mousePresionado = false;
+
+document.addEventListener('mousedown', () => {
+    mousePresionado = true;
+});
+document.addEventListener('mouseup', () => {
+    mousePresionado = false;
+});
+
+
+
+
 for (let index = 0; index < 256; index++) {
     const div = document.createElement('div');
     div.classList.add('blank');
@@ -7,7 +22,15 @@ for (let index = 0; index < 256; index++) {
 }
 const divsInsideContainer = document.querySelectorAll('.blank');
 divsInsideContainer.forEach(div => {
-    div.addEventListener('mouseover', () => {
+
+    div.addEventListener('mousedown', () => {
         div.classList.add('change-background-color-on-hover');
+    });
+
+    div.addEventListener('mouseover', () => {
+        if (mousePresionado){
+            div.classList.add('change-background-color-on-hover');
+        };
+
     })
 });
